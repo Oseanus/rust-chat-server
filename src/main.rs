5b79ff77cb8efb9125bc82556ac6e2e83ceef986
Oliver Ellis <ellis.oliver@gmail.com>
2023-01-08 15:11:56 +0100

    Added tasks for multible clients

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-// mod server;
-
 use tokio::{
     net::TcpListener, 
-    io::{AsyncReadExt, AsyncWriteExt}
+    io::{AsyncWriteExt, BufReader, AsyncBufReadExt}
 };
 
 #[tokio::main]
@@ -10,12 +8,25 @@ async fn main() {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).await.unwrap();
 
-    let (mut socket, socketAddr) = listener.accept().await.unwrap();
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
 
-    loop{
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        
+        tokio::spawn(async move {
+            let (read, mut writer) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+            
+            loop{
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+    
+                if bytes_read == 0 {
+                    break;
+                }
+    
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
