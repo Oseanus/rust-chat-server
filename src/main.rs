f1cfbb82cf80897f4b7a946263ca1d9195fe13c6
Oliver Ellis <ellis.oliver@gmail.com>
2023-01-08 14:46:15 +0100

    Added functionality as echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+// mod server;
+
+use tokio::{
+    net::TcpListener, 
+    io::{AsyncReadExt, AsyncWriteExt}
+};
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    let (mut socket, socketAddr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8; 1024];
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();
 }
